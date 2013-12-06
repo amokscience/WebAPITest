dbde1ee61d6a879c6b4ef1b7b9228be7e0c39fa7
Ken <kenito@murp.org>
2013-12-06 00:23:59 -0600

    Added SnackUser

---
+++ WebAPITest/Controllers/SnackUserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPITest.Controllers
+{
+    public class SnackUserController : ApiController
+    {
+        // GET api/snackuser
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/snackuser/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/snackuser
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/snackuser/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/snackuser/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
